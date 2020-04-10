a092d208d0fc37ba74d3670ca8c212555811f0cc
kd0104 <63348860+kd0104@users.noreply.github.com>
2020-04-10 22:55:25 +0530

    editing_list.py
    basic editing and printing of a list data structure

---
+++ editing_list.py
@@ -0,0 +1,35 @@
+num1=[20,22,17,16]
+num2=[2017,2017,2020,2020]
+names=['jones','jack']
+
+
+print(num1)
+print(num2)
+print(names)
+
+
+#for concatination:
+
+print(num1+num2)
+
+#for calling elements from index values of a list
+
+print(num1[0])
+print(num2[0])
+print(num1[0:])
+print(num2[0:])
+
+
+#for deleting:
+
+del num1[2]
+print(num1)
+
+del num2[2]
+print(num2)
+
+
+#for adding
+
+num1.append[1]
+print(num1)
