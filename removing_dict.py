e9ed3f2673bce5c649aa384a514519353bb341d2
kd0104 <63348860+kd0104@users.noreply.github.com>
2020-04-10 23:16:41 +0530

    removing_dict.py
    code to remove element in dict

---
+++ removing_dict.py
@@ -0,0 +1,10 @@
+myDict = {
+    "principal":  "headmaster"
+    "faculty":  "master"
+    "student":  "slave"
+    "just":  "kidding"
+    }
+#to remove a element
+
+del myDict["student"]
+print(myDict);
